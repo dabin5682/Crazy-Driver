cb9fe8b41f8ef94851eda3e5e254b13b31ab137f
dabin5682 <dabin5682@gmail.com>
2024-09-20 18:00:49 +0900

    init

---
+++ CrazyDriver/Main.py
@@ -0,0 +1,15 @@
+#불러오기
+import pygame
+
+#게임 색상 정의
+
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+RED = (255,0,0)
+
+#게임 시작은 이곳에서
+#파이 게임 초기화 하기
+pygame.init()
+#프레임 매니저 초기화하기
+clock = pygame.time.Clock()
+#프레임 매니저 설정하기
