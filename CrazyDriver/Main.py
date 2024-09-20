334d2e3370784b657d22ef71bb42d47f33f62665
dabin5682 <dabin5682@gmail.com>
2024-09-20 20:26:07 +0900

    main03

--- CrazyDriver/Main.py
+++ CrazyDriver/Main.py
@@ -12,4 +12,12 @@ RED = (255,0,0)
 pygame.init()
 #프레임 매니저 초기화하기
 clock = pygame.time.Clock()
-test
+#프레임 레이트 설정하기
+clock.tick(60)
+
+#제목 표시줄 설정
+pygame.display.set_caption("Crazy Driver")
+
+screen = pygame.display.set_mode((500,800))
+screen.fill(WHITE)
+pygame.display.update()
