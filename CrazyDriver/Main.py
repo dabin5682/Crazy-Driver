5f10a51d1e5c2e19a4bd682b1b84fa60e9ac17d2
dabin5682 <dabin5682@gmail.com>
2024-09-29 22:31:51 +0900

    혹시몰라

--- CrazyDriver/Main.py
+++ CrazyDriver/Main.py
@@ -6,6 +6,7 @@ from pygame.locals import *
 BLACK = (0,0,0)
 WHITE = (255,255,255)
 RED = (255,0,0)
+#변수 초기화
 paused = False
 moveSpeed = 5
 startSpeed = 5
@@ -45,31 +46,22 @@ def GameOver():
 
     pygame.quit()
     sys.exit()
+
 #게임 시작은 이곳에서
 #파이 게임 초기화 하기
 pygame.init()
 #프레임 매니저 초기화하기
 clock = pygame.time.Clock()
+#프레임 레이트 설정하기
 clock.tick(60)
+
 #제목 표시줄 설정
 pygame.display.set_caption("Crazy Driver")
 
 #이미지 불러오기
 IMG_ROAD =pygame.image.load(os.path.join(IMAGE_FOLDER, "Road.png"))
 IMG_PLAYER =pygame.image.load(os.path.join(IMAGE_FOLDER,"Player.png"))
-IMG_Oil =pygame.image.load(os.path.join(IMAGE_FOLDER,"Oil.png"))
-IMG_Oil_width = 300
-IMG_Oil_height = 300
-IMG_Oil = pygame.transform.scale(IMG_Oil, (IMG_Oil_width, IMG_Oil_height))
-#화면을 방해할 오일 위치설정
-oil_x = (IMG_ROAD.get_width() - IMG_Oil_width) // 2
-oil_y = (IMG_ROAD.get_height() - IMG_Oil_height) // 2
-IMG_ENEMIES =[]
-IMG_ENEMIES.append(pygame.image.load(os.path.join(IMAGE_FOLDER,"Enemy.png")))
-IMG_ENEMIES.append(pygame.image.load(os.path.join(IMAGE_FOLDER,"Enemy2.png")))
-IMG_ENEMIES.append(pygame.image.load(os.path.join(IMAGE_FOLDER,"Enemy3.png")))
-IMG_ENEMIES.append(pygame.image.load(os.path.join(IMAGE_FOLDER,"IceCube.png")))
-IMG_ENEMIES.append(pygame.image.load(os.path.join(IMAGE_FOLDER,"Oil.png")))
+IMG_ENEMY =pygame.image.load(os.path.join(IMAGE_FOLDER,"Enemy.png"))
 #게임화면 초기화하기
 screen = pygame.display.set_mode(IMG_ROAD.get_size())
 
@@ -77,19 +69,41 @@ screen = pygame.display.set_mode(IMG_ROAD.get_size())
 #플레이어 초기위치 설정
 h=IMG_ROAD.get_width()//2
 v=IMG_ROAD.get_height()-(IMG_PLAYER.get_height()//2)
+
 #플레이어 스프라이트 만들기
 player = pygame.sprite.Sprite()
 player.image = IMG_PLAYER
 player.surf = pygame.Surface(IMG_PLAYER.get_size())
 player.rect = player.surf.get_rect(center = (h,v))
+
+#적
+IMG_ENEMIES =[]
+IMG_ENEMIES.append(pygame.image.load(os.path.join(IMAGE_FOLDER,"Enemy.png")))
+IMG_ENEMIES.append(pygame.image.load(os.path.join(IMAGE_FOLDER,"Enemy2.png")))
+IMG_ENEMIES.append(pygame.image.load(os.path.join(IMAGE_FOLDER,"Enemy3.png")))
+IMG_ENEMIES.append(pygame.image.load(os.path.join(IMAGE_FOLDER,"IceCube.png")))
+IMG_ENEMIES.append(pygame.image.load(os.path.join(IMAGE_FOLDER,"Oil.png")))
+#적 초기위치 설정
+hl= IMG_ENEMY.get_width()//2
+hr = IMG_ROAD.get_width()-(IMG_ENEMY.get_width()//2)
+h = random.randrange(hl,hr)
+v=0
+#적 스프라이트 설정
+enemy = pygame.sprite.Sprite()
+enemy.image = IMG_ENEMY
+enemy.surf = pygame.Surface(IMG_ENEMY.get_size())
+enemy.rect = enemy.surf.get_rect(center = (h,v))
+
+
+
 while True:
-    clock.tick(60)
-    #배경화면 설정
-    pygame.display.set_caption("Crazy Driver-Score "+str(score))
+    #배경화면
     screen.blit(IMG_ROAD,(0,0))
     #캐릭터 화면 배치
     screen.blit(player.image,player.rect)
-    #적 설정
+    screen.blit(enemy.image,enemy.rect)
+    keys = pygame.key.get_pressed()
+    #적설정
     if eNum == -1:
         eNum = random.randrange(0,len(IMG_ENEMIES))
         hl= IMG_ENEMIES[eNum].get_width()//2
@@ -105,7 +119,6 @@ while True:
     screen.blit(enemy.image,enemy.rect)
     #적을 아래로 움직이기
     enemy.rect.move_ip(0,moveSpeed)
-    
     if eNum == -1:
         eNum = random.randrange(0,len(IMG_ENEMIES))
         hl= IMG_ENEMIES[eNum].get_width()//2
@@ -125,8 +138,7 @@ while True:
         score += 1
         if moveSpeed <maxSpeed:
             moveSpeed +=1
-    keys = pygame.key.get_pressed()
-    #일시정지
+     #일시정지
     if paused:
         if not keys[K_SPACE]:
             #일시정지 종료
@@ -147,27 +159,18 @@ while True:
             tempSpeed = moveSpeed
             moveSpeed = 0
             paused = True
-    #충돌 확인
-    if eNum> 0 and pygame.sprite.collide_rect(player,enemy):
-        # 얼음장애물일때
-        if eNum == 3:
-            moveSpeed =  startSpeed
-            #충돌시 게임오버
-        # 오일장애물일때
-        elif eNum == 4:
-            if overlay_start_time is None:
-                overlay_start_time = time.time() #충돌시간 기록
 
-            if time.time() - overlay_start_time < 5:
-                screen.blit(IMG_Oil,(oil_x,oil_y))
-            else:
-                overlay_start_time = None           
-        else: 
-            GameOver()
     for event in pygame.event.get():
         #게임을 그만두나요?
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+    #충돌 확인
+    if eNum> 0 and pygame.sprite.collide_rect(player,enemy):
+        # 얼음장애물일때
+        if eNum == 3:
+            moveSpeed =  startSpeed           
+        else: 
+            GameOver()
+
     pygame.display.update()
-  
